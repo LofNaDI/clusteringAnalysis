de9dde5dd9c98bea9ce0e0f449485cfdf6872793
Salva Ardid <jsardid@gmail.com>
2017-11-21 16:17:40 -0500

    old pending changes

--- main_kmeans_aicbic.m
+++ main_kmeans_aicbic.m
@@ -10,16 +10,19 @@ type = 'Kmeans';
 % control cluster data checked clusters from 5:10 (offset = 4), the relevant number of clusters is 7
 offset = 4;
 minNumOfElementsCluster = 5;
-for k = 5:15 % num of clusters
+numC = 5:14; % num of clusters
+for k = numC
   relIndex = k-offset;
   tmp = load([suffix,'/',type,'ClusteringResults']);
   control.data = tmp.datanorm4Cluster;
   numDim = size(control.data,2);
   nElements = length(tmp.labels);
   control.labels = nan(1,nElements);
-  control.clustOrder = tmp.clustOrder;
+  % control.clustOrder = tmp.clustOrder;
   cont = 0;
-  for i = control.clustOrder
+  Nc = [];
+  Vc = [];
+  for i = 1:length(tmp.clustFilt) % control.clustOrder
     if (length(tmp.clustFilt{relIndex,i}) < minNumOfElementsCluster)
      continue
     else
@@ -29,15 +32,16 @@ for k = 5:15 % num of clusters
       control.elemInCluster{cont} = tmp.clustFilt{relIndex,i};
       Nc(cont) = length(control.elemInCluster{cont});
       Vc(:,cont) = var(control.data(control.labels==cont,:),[],1); % normalizing by n-1, there is no cluster with only 1 element
+      Vc(Vc(:,cont) == 0, cont) = nan;
     end
   end
-  clear tmp;
+  % clear tmp;
 
   numRelClusters = cont;
 
   % V = var(control.data,[],1)'*ones(1,numRelClusters); % normalizing by n-1, there is no cluster with only 1 element
-  % logL = -Nc.*sum(.5*log(Vc+V),1);
-  logL = -Nc.*sum(.5*log(Vc),1); % there is no cluster with only 1 element
+  % logL = -Nc.*nansum(.5*log(Vc+V),1);
+  logL = -Nc.*nansum(.5*log(Vc),1); % there is no cluster with only 1 element
   aic(relIndex) = -2 * sum(logL) + 4*numRelClusters*numDim;
   bic(relIndex) = -2 * sum(logL) + 2*numRelClusters*numDim*log(nElements);
 end

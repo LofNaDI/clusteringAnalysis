c60492887f6a62d57707bf0d26805c29b95294ca
Salva Ardid <jsardid@gmail.com>
2019-02-09 00:03:18 -0500

    fixed a bug in the mapping between the dendrogram and the kmeans results

--- main_dendrogram_clustersCentroids.m
+++ main_dendrogram_clustersCentroids.m
@@ -38,7 +38,7 @@ end
 Ap = [];
 for i = 1:k
   % I add a term only to keep the proper structure of the dendrogram according to the neurons in the clusters
-  Ap = [Ap;(ones(numElem(i),1)+0.01*((1:numElem(i))'/numElem(i)-0.5))*centers(i,:)];
+  Ap = [Ap;(ones(numElem(i),1)+1e-7*((1:numElem(i))'/numElem(i)-0.5))*centers(i,:)];
 end
 Y = pdist(Ap);
 Z = linkage(Y,'average');
@@ -59,11 +59,26 @@ xlabel('Distance','fontsize',16);
 set(gca,'fontSize',16,'LineWidth',1,'TickDir','out','Box','off','XTick',0:.2:1,'YTick',[])
 plot2svg([resultsdir,'/dendrogram_',type,'_centroids.svg'])
 
-% matching the dendrogram with the results of the kmeans to determine the cluster ordering
-clustersNumOfElements = find(diff(c(outperm))~=0);
-clustersNumOfElements = [clustersNumOfElements(1);diff([clustersNumOfElements;length(c)])];
-for i = 1:length(clustersNumOfElements)
-  clustOrder(i) = find(clustersNumOfElements(i)==numElem);
-end
+%% matching the dendrogram with the results of the kmeans to determine the cluster ordering
+%% doing so by matching the number of elements (warning!!! it is not reliable if two or more clusters have the same number of elements)
+% clustersNumOfElements = find(diff(c(outperm))~=0);
+% clustersNumOfElements = [clustersNumOfElements(1);diff([clustersNumOfElements;length(c)])];
+% for i = 1:length(clustersNumOfElements)
+%   clustOrder(i) = find(clustersNumOfElements(i)==numElem);
+% end
+
+%% matching the dendrogram with the results of the kmeans to determine the cluster ordering
+%% doing so by checking the distance between centers (note: a reliable way to do it and useful for debugging)
+% for i = 1:size(centers,1)
+%   X = centers;
+%   Y = mean(Ap(c==i,:),1);
+%   idx(i) = find(pdist2(X,Y,'euclidean') <= 1e-6);
+% end
+
+%% matching the dendrogram with the results of the kmeans to determine the cluster ordering
+%% the proper way to do it remapping indexes
+[~,idx] = sort(unique(c','stable'));
+
+clustOrder = idx(unique(c(outperm)','stable'));
 
 save(matfile,'clustFilt','clustOrder','-append');
